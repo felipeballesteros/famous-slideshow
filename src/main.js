c617a2804f4ffa21ebc450b14518fe3ce568c0e0
felipeballesteros <felipeballesteros@msn.com>
2014-06-10 17:49:07 -0400

    adding helper to get images URLs

--- src/main.js
+++ src/main.js
@@ -1,15 +1,31 @@
 define(function(require, exports, module) {
 	var Engine  = require('famous/core/Engine');
 
+	// import Utility
+	var Utility = require('famous/utilities/Utility');
+
 	// import the app view using require
 	var AppView = require('views/AppView');
 
+	// import SlideData
+	var SlideData = require('data/SlideData');
+
 	var mainContext = Engine.createContext();
 
-	// create a new instance of AppView
-	var appView = new AppView();
+	function initApp(data) {
 
-	// add the instance to the mainContext
-	mainContext.add(appView);
+		// parses out response data and retrieves arrays of URLs
+		data = SlideData.parse(data);
 
+		// instatiates AppView with our URL data
+		var appView = new AppView({
+			data: data
+		});
+
+		// add the instance to the mainContext
+		mainContext.add(appView);
+	}
+
+	// simple Get request to the Picasa API with a callback
+	Utility.loadURL(SlideData.getUrl(), initApp);
 });
